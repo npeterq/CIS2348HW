87568ba34a52129d900b8cf9ddf07c623df1bb6b
npeterq <85538898+npeterq@users.noreply.github.com>
2021-06-14 08:40:35 -0500

    Create hw#1.py

---
+++ hw#1.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+
+print("Birthday Calculator")
+print("Current Day")
+month = int(input("Month: "))
+day = int(input("Day: "))
+year = int(input("Year: "))
+
+print("Birthday")
+bmonth = int(input("Month: "))
+bday = int(input("Day: "))
+byear = int(input("Year: "))
+print("You are", year - byear - ((month, day) < (bmonth, bday)), "years old.")
+if day == bday and month == bmonth:
+    print("Happy Birthday!")
