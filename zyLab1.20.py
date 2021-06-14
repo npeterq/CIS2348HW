dcfc9c7d2a88ff8b3f0ea0eff23d421d34584309
npeterq <85538898+npeterq@users.noreply.github.com>
2021-06-14 08:57:40 -0500

    Add files via upload

---
+++ zyLab1.20.py
@@ -0,0 +1,15 @@
+"Peter Nguyen"
+"6/14/2021"
+"CIS2348"
+"1860823"
+"1.20 CIS 2348 LAB: Warm up: Basic output with variables"
+
+user_num = int(input("Enter integer:\n"))
+
+# Print the output
+print("You entered:", user_num)
+print(f"{user_num} squared is", user_num * user_num)
+print(f"And {user_num} cubed is {user_num * user_num * user_num} !!")
+user_num2 = int(input("Enter another integer:\n"))
+print(f"{user_num} + {user_num2} is", user_num + user_num2)
+print(f"{user_num} * {user_num2} is", user_num * user_num2)
