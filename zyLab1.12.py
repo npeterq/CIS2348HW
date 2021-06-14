476ab7296536031cf7d5b1a5d0481626952509ea
npeterq <85538898+npeterq@users.noreply.github.com>
2021-06-14 08:52:26 -0500

    Add files via upload

---
+++ zyLab1.12.py
@@ -0,0 +1,10 @@
+"Peter Nguyen"
+"6/14/2021"
+"CIS2348"
+"1860823"
+"1.12 CIS 2348 zyLab training: Basics"
+
+userNum = int(input())
+userNumSquared = userNum*userNum
+print(userNumSquared, end='')
+print("")
